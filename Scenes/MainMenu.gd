0454f16e1ff718c52d0e49f582729bf26aec6fbf
Frances <frances.calceta@gmail.com>
2022-03-19 22:04:03 +0800

    refactored code, added a timer for better looks

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -17,31 +17,39 @@ func _ready():
 
 func _process(delta):
 	if currentWord >= typeList.size():
+		#temporary, should be change to next level
+		get_tree().reload_current_scene()
 		pass
 	else:
+		typeLetter()
 		decorateType()
 		
-func _unhandled_input(event: InputEvent):
-	# make sure it's not a shift or another character
-	if str(TypingHandler.currentKey).to_lower() in ["a", "b", "c", "d", "e", "f", "g", 
-	"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", 
-	"w", "x", "y", "z"]:
-		var current = str(TypingHandler.currentKey)
+func typeLetter():
+	
+	if lastTypedIndex < typeList[currentWord].length():
+		# make sure it's not a shift or another character
+		if str(TypingHandler.currentKey).to_lower() in ["a", "b", "c", "d", "e", "f", "g", 
+		"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", 
+		"w", "x", "y", "z"]:
+			var current = str(TypingHandler.currentKey)
 			
-		if currentWord >= typeList.size():
-			#FINISHED LEVEL
-			pass
-		elif lastTypedIndex >= typeList[currentWord].length()-1:
-			currentWord += 1
-			lastTypedIndex = 0
-		elif current == typeList[currentWord][lastTypedIndex]:
-			lastTypedIndex += 1
-		else:
-			lastTypedIndex = 0
-		
-		print(str(TypingHandler.currentKey), lastTypedIndex)
+			if current == typeList[currentWord][lastTypedIndex]:
+				lastTypedIndex += 1
+			else:
+				lastTypedIndex = 0
+			
+			print(str(TypingHandler.currentKey), lastTypedIndex)
+	elif $NextWordTimer.is_stopped():
+		$NextWordTimer.start()
+		# when the timer times out, reset the indexes
 
 func decorateType() -> void:
 	var colorStart = "[color=#FFAAAA]"
 	var colorEnd =  "[/color]"
 	$ToType.bbcode_text = typeList[currentWord].substr(0, lastTypedIndex)+colorStart+typeList[currentWord].substr(lastTypedIndex, -1)+colorEnd
+
+
+func _on_NextWordTimer_timeout():
+	lastTypedIndex = 0
+	currentWord += 1
+	pass # Replace with function body.
